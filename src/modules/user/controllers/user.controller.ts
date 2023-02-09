9ff853f5b0a1f140f924f227fed4f28792847a71
iamroms <rcena@startechup.com>
2023-02-09 13:45:51 +0800

    - tmp returned user

--- src/modules/user/controllers/user.controller.ts
+++ src/modules/user/controllers/user.controller.ts
@@ -212,11 +212,42 @@ export class UserController {
       };
     }
 
+    const tmpUser: any = {
+        id: 1,
+        firstName: 'Chris',
+        lastName: 'Johnson',
+        imgUrl: 'https://via.placeholder.com/24x24',
+        userName: '@john1989',
+        email: {
+          name: 'chris.johnson@altence.com',
+          verified: false,
+        },
+        phone: {
+          number: '+18143519459',
+          verified: true,
+        },
+        sex: 'male',
+        birthday: '01/26/2022',
+        lang: 'en',
+        country: 'GB',
+        city: 'London',
+        address1: '14 London Road',
+        zipcode: 5211,
+        website: 'altence.com',
+        socials: {
+          twitter: '@altence_team',
+          facebook: 'https://facebook.com/groups/1076577369582221',
+          linkedin: 'https://linkedin.com/company/altence',
+        },
+      };
+
     return {
       tokenType,
       expiresIn,
       accessToken,
       refreshToken,
+      token: accessToken,
+      user: tmpUser
     };
   }
 
