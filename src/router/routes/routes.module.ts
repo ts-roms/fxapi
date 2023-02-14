21aca5aea1eadb5a9c2af4e84355ad087264c5b2
iamroms <rcena@startechup.com>
2023-02-14 20:58:32 +0800

    - update folder

--- src/router/routes/routes.module.ts
+++ src/router/routes/routes.module.ts
@@ -6,10 +6,10 @@ import { MessageController } from 'src/common/message/controllers/message.contro
 import { SettingController } from 'src/common/setting/controllers/setting.controller';
 import { HealthController } from 'src/health/controllers/health.controller';
 import { HealthModule } from 'src/health/health.module';
-import { BranchModule } from 'src/modules/branch/branch.module';
-import { BranchController } from 'src/modules/branch/controllers/branch.controller';
-import { EmployeeController } from 'src/modules/employee/controllers/employee.controller';
-import { EmployeeModule } from 'src/modules/employee/employee.module';
+import { BranchModule } from 'src/modules/system/branch/branch.module';
+import { BranchController } from 'src/modules/system/branch/controllers/branch.controller';
+import { EmployeeController } from 'src/modules/system/employee/controllers/employee.controller';
+import { EmployeeModule } from 'src/modules/system/employee/employee.module';
 import { RoleModule } from 'src/modules/role/role.module';
 import { UserController } from 'src/modules/user/controllers/user.controller';
 import { UserModule } from 'src/modules/user/user.module';
